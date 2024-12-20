33dc61a4cc79613a9330c44d057239041bc88002
hevorange <hevonox@gmail.com>
2024-12-20 17:08:28 -0600

    cambios y correciones en el Readme.md, gitignore  y main.py

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Arkon Data - Prueba Técnica
+
+## INSTRUCCIONES PARA EJECUTAR
+
+
+1. Navega a la carpeta del proyecto
+```
+cd ArkonData_Prueba
+```
+
+2. crea un entorno virtual e instala las dependencias requeridas con el archivo requirements.txt:
+
+```
+python3 -m venv env
+pip3 install -r requirements.txt
+source env/bin/activate
+```
+
+3. Ejecuta el archivo main.py
+```
+python3 main.py
+```
--- main.py
+++ main.py
@@ -7,16 +7,22 @@ parquet_path = './data/data2.parquet'
 db_name = "arkon.db"
 table_name ="person"
 
-df = etl_functions.extract(csv_path, parquet_path)
-# df_starships_unique = etl_functions.get_unique_values(df)
-# print(df_starships_unique)
+def run():
 
-# print(etl_functions.record_counts(df))
+    df = etl_functions.extract(csv_path, parquet_path)
+    # df_starships_unique = etl_functions.get_unique_values(df)
+    # print(df_starships_unique)
 
-# print(etl_functions.get_duplicate_names(df))
+    # print(etl_functions.record_counts(df))
 
-sql_connection = sqlite3.connect(db_name)
-etl_functions.load_to_db(df, sql_connection, table_name)
+    # print(etl_functions.get_duplicate_names(df))
 
-query_statement = f"SELECT * FROM {table_name}"
-etl_functions.run_query(query_statement,sql_connection)
+    sql_connection = sqlite3.connect(db_name)
+    etl_functions.load_to_db(df, sql_connection, table_name)
+
+    query_statement = f"SELECT * FROM {table_name}"
+    etl_functions.run_query(query_statement,sql_connection)
+
+
+if __name__=='__main__':
+    run()
