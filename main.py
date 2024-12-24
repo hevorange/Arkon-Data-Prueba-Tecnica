9747481987ad3a5bf82abadd45501ba1687470ee
hevorange <hevonox@gmail.com>
2024-12-23 21:52:17 -0600

    terminamos todos los pasos de la prueba tecnica

--- main.py
+++ main.py
@@ -25,11 +25,22 @@ def run():
         height arriba de 180 pero menor a 190, que cumplan la condición de ser male y el hair_color
         sea cualquiera menos “none”
 
-    query_statement = f"SELECT * FROM {table_name} WHERE height > 180 AND height < 190 AND sex='male' AND hair_color!='none'"
+    query_statement = f"SELECT name,height,hair_color,sex FROM {table_name} WHERE height > 180 AND height < 190 AND sex='male' AND hair_color!='none'"
     etl_functions.run_query(query_statement,sql_connection)
     
     """
 
+# PASO 6 - Generar Banderas: 1 <-> el mass está arriba del promedio y 1 <-> el mass está arriba del promedio
+
+    """query_statement =f"WITH avg_mass AS ( SELECT AVG(mass) AS avg_mass FROM {table_name} ) SELECT mass, CASE WHEN mass > (SELECT avg_mass FROM avg_mass) THEN 1 ELSE 0 END AS flag FROM {table_name} LIMIT 20;"
+    etl_functions.run_query(query_statement,sql_connection)"""
+
+
+    query_statement=f"SELECT species, AVG(height) AS avg_height, MAX(height) AS max_height, MIN(height) AS min_height FROM person GROUP BY species;"
+    etl_functions.run_query(query_statement, sql_connection)
+
+# PASO 7 alcular la altura promedio, la altura máxima y mínima por especie, mediante una sentencia SQL
+    
     
 
 
